bd57b38e68588b424938591efb78a5d89db8b345
ManNjoro <elijohnmwoho@gmail.com>
2025-07-29 23:25:33 +0300

    Implement navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const navItems = [
+    {label: 'Home', href:'/'},
+    {label: 'Companions', href:'/companions'},
+    {label: 'My journey', href:'/my-journey'},
+]
+
+const NavItems = () => {
+    const pathname = usePathname() // whenever you use a hook - use client
+  return (
+    <nav className='flex items-center gap-4'>
+        {navItems.map(({label, href}) => (
+            <Link href={href} key={label} className={cn(pathname === href && 'text-primary font-semibold')}>{label}</Link>
+        ))}
+    </nav>
+  )
+}
+
+export default NavItems
